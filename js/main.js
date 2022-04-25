adc16cca48326d3771d80a27d62c0766e06501ee
bomb703 <70813850+bomb703@users.noreply.github.com>
2022-04-26 00:55:49 +0300

    Add files via upload
    Updated:
    ● Adaptive:
      - Added smooth scroll script for anchors
      - Redone header section
      -  Adaptive main page for PC, laptops, phones, tablet and others
    ● Map:
      - Added new places to the map
      - Added Sports grounds photos in map
      - Added new marker icons

--- js/main.js
+++ js/main.js
@@ -40,40 +40,17 @@ for (let anchor of anchors) {
 
 // Якорь
 
-// map
+// бэргер
 
-function initMap() {
-    var element = document.getElementById('map');
-    var options = {
-        zoom: 12,
-        center: { lat: 59.38041044161922, lng: 28.175735864389836 }
-    };
-
-    var myMap = new google.maps.Map(element, options);
-
-    addMarker({ lat: 59.37983574128731, lng: 28.186255232642093 });
-    addMarker({ lat: 59.38528001107806, lng: 28.192306656464986 });
-    addMarker({ lat: 59.37397745900335, lng: 28.20174929310566 });
-    addMarker({ lat: 59.377300010389966, lng: 28.20278615435148 });
-    addMarker({ lat: 59.38458783676808, lng: 28.18491753317914 });
-    addMarker({ lat: 59.37474126301563, lng: 28.170214673403088 });
-    addMarker({ lat: 59.364867932199466, lng: 28.18172660308546 });
-    addMarker({ lat: 59.36299813821562, lng: 28.184054760411623 });
-    addMarker({ lat: 59.393558840515205, lng: 28.159595304969766 });
-    addMarker({ lat: 59.380855086542404, lng: 28.18850911367739 });
-    addMarker({ lat: 59.39109946941314, lng: 28.173919536073733 });
-    addMarker({ lat: 59.38788540920943, lng: 28.16978004669319 });
-    addMarker({ lat: 59.378912459689076, lng: 28.18497360659935 });
-    addMarker({ lat: 59.380338411828916, lng: 28.16686844071822 });
-    // addMarker({ lat: , lng: }); 
-
-    function addMarker(coordinates) {
-        var Mymap = new google.maps.Marker({
-            position: coordinates,
-            map: myMap,
-        })
-    }
-
-
+const BurgerMenu = document.querySelector('.menu-burger');
+if (BurgerMenu) {
+    const menuBody = document.querySelector('.menu-body');
+    BurgerMenu.addEventListener("click", function (e) {
+        document.body.classList.toggle('_lock');
+        BurgerMenu.classList.toggle('header-active')
+        menuBody.classList.toggle('header-active')
+    });
 }
-// map
+
+// бэргер
+
---
+++ js/map.js
@@ -0,0 +1,157 @@
+// map
+function initMap() {
+    var element = document.getElementById('map');
+    var options = {
+        zoom: 13,
+        center: { lat: 59.38041044161922, lng: 28.175735864389836 }
+    }
+
+    var myMap = new google.maps.Map(element, options);
+
+    var markers = [
+        {
+            coordinates: { lat: 59.37983574128731, lng: 28.186255232642093 },
+            image: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+            info: '<h1 class="title-map">MyFitness</h1> <img src="img/myfitness.jpg">'
+        },
+        {
+            coordinates: { lat: 59.385208126697144, lng: 28.190759412664068 },
+            image: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+            info: '<h1 class="title-map">SpordiKeskus</h1> <img src="img/spordikeskus.jpg">'
+        },
+        {
+            coordinates: { lat: 59.374014395693486, lng: 28.20167136717099 },
+            image: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+            info: '<h1 class="title-map"> Narva Linna Arendus</h1> <img src="img/lipovka.jpg">'
+        },
+        {
+            coordinates: { lat: 59.38004564910027, lng: 28.189047755624916 },
+            image: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+            info: '<h1 class="title-map">Narva Baza</h1> <img src="img/baza.jpg">'
+        },
+        {
+            coordinates: { lat: 59.38013, lng: 28.1671 },
+            info: '<img src="img/ex2.jpg">'
+        },
+        {
+            coordinates: { lat: 59.38001, lng: 28.16254 },
+            info: '<img src="img/ex3.jpg">'
+        },
+        {
+            coordinates: { lat: 59.38612, lng: 28.16425 },
+            info: '<img src="img/ex4.jpg">'
+        },
+        {
+            coordinates: { lat: 59.39216, lng: 28.16608 },
+            info: '<img src="img/ex5.jpg">'
+        },
+        {
+            coordinates: { lat: 59.39376, lng: 28.1605 },
+            info: '<img src="img/ex6.jpg">'
+        },
+        {
+            coordinates: { lat: 59.39221, lng: 28.17507 },
+            info: '<img src="img/ex7.jpg">'
+        },
+        {
+            coordinates: { lat: 59.39087, lng: 28.17784 },
+            info: '<img src="img/ex8.jpg">'
+        },
+        {
+            coordinates: { lat: 59.38945, lng: 28.17111 },
+            info: '<img src="img/ex9.jpg">'
+        },
+        {
+            coordinates: { lat: 59.38764, lng: 28.16939 },
+            info: '<img src="img/ex10.jpg">'
+        },
+        {
+            coordinates: { lat: 59.37964, lng: 28.17515 },
+            info: '<img src="img/ex11.jpg">'
+        },
+        {
+            coordinates: { lat: 59.37447, lng: 28.16994 },
+            info: '<img src="img/ex12.jpg">'
+        },
+        {
+            coordinates: { lat: 59.3652, lng: 28.17905 },
+            info: '<img src="img/ex13.jpg">'
+        },
+        {
+            coordinates: { lat: 59.37387, lng: 28.19979 },
+            info: '<img src="img/ex14.jpg">'
+        },
+        {
+            coordinates: { lat: 59.37377, lng: 28.20095 },
+            info: '<img src="img/ex15.jpg">'
+        },
+        {
+            coordinates: { lat: 59.37989, lng: 28.20454 },
+            info: '<img src="img/ex16.jpg">'
+        },
+        {
+            coordinates: { lat: 59.36357, lng: 28.1845 },
+            info: '<img src="img/ex17.jpg">'
+        },
+        {
+            coordinates: { lat: 59.36601, lng: 28.19214 },
+            info: '<img src="img/ex18.jpg">'
+        },
+        {
+            coordinates: { lat: 59.38758, lng: 28.19318 },
+            info: '<img src="img/ex19.jpg">'
+        },
+        {
+            coordinates: { lat: 59.3851, lng: 28.17213 },
+            info: '<img src="img/ex20.jpg">'
+        },
+        {
+            coordinates: { lat: 59.384567004474576, lng: 28.184972182608977 },
+            info: '<img src="img/ex22.jpg">'
+        },
+        {
+            coordinates: { lat: 59.38087697189372, lng: 28.188540740699942 },
+            info: '<img src="img/ex21.jpg">'
+        },
+        {
+            coordinates: { lat: 59.38093586130014, lng: 28.186387158782445 },
+            info: '<img src="img/.jpg">'
+        },
+        {
+            coordinates: { lat: 59.37859, lng: 28.17002 },
+            info: '<img src="img/ex1.jpg">'
+        },
+    ];
+
+    for (var i = 0; i < markers.length; i++) {
+        addMarker(markers[i]);
+    }
+
+
+    addMarker();
+    addMarker();
+
+
+    function addMarker(properties) {
+        var marker = new google.maps.Marker({
+            position: properties.coordinates,
+            map: myMap,
+        });
+
+        if (properties.image) {
+            marker.setIcon(properties.image);
+        }
+
+        if (properties.info) {
+            var InfoWindow = new google.maps.InfoWindow({
+                content: properties.info
+            });
+
+            marker.addListener('click', function () {
+                InfoWindow.open(myMap, marker);
+            })
+        }
+
+    }
+}
+// map
